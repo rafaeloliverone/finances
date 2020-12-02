f81ffafcba77de881406640065fd68ce194bec34
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-12-01 22:57:23 -0300

    adding svg to page Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App:React.FC = () => {
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Hello</h1>
+      <GlobalStyle />
+    </>
   );
 }
 
