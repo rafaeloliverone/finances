2172bd581868913d89b4b33cd6759e497ea35466
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-12-03 23:58:12 -0300

    creating themes dark and light

---
+++ frontend/src/theme/dark.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#5A5A66',
+    secondary: '#2A2B2E',
+
+    background: '#C6C6C6',
+
+    text: '#FFF',
+  },
+
+  buttonColors: {
+    primary: '#87FF65',
+    secondary: '#5A5A66',
+  }
+
+}
---
+++ frontend/src/theme/light.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#334195',
+    secondary: '#647AA3',
+
+    background: '#C6EBBE',
+
+    text: '#FFF',
+  },
+
+  buttonColors: {
+    primary: '#95B2B0',
+    secondary: '#C6EBBE',
+  }
+
+}
