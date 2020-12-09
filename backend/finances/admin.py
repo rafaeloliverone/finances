7cad7cca562f3c0f0f1d0cfa17fa50287e69a1a6
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-12-09 00:11:43 -0300

    creating choices for model Card

--- backend/finances/admin.py
+++ backend/finances/admin.py
@@ -5,7 +5,7 @@ from .models import Card, Category
 
 class CardAdmin(admin.ModelAdmin):
     list_display = ('name', 'flag', 'category', 'owner', )
-    fields = ('name', 'flag', 'category', 'last_number', 'closing_day', 'owner', )
+    fields = ('name', 'flag', 'category', 'last_numbers', 'closing_day', 'owner', )
 
 
 admin.site.register(Card, CardAdmin)
---
+++ backend/utils/models.py
@@ -0,0 +1,16 @@
+FLAG_CHOICES = (
+    ('VISA', 'Visa'),
+    ('HIPER', 'Hipercard'),
+    ('SANTANDER', 'Santander'),
+    ('MASTERCARD', 'MasterCard'),
+    ('ELO', 'Elo'),
+    ('NUBANK', 'Nubank'),
+    ('INTER', 'Inter'),
+)
+
+CATEGORY_CARD_CHOICES = (
+    ('DEBIT', 'Débito'),
+    ('CREDIT', 'Crédito'),
+    ('BOTH', 'Débito e Crédito'),
+)
+
