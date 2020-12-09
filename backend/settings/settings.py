0f8f8c0c879305f36b1d30a5848bdee07fc18363
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-12-08 23:58:46 -0300

    adding new apps in settings and creating UserAdmin with custom forms

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts.apps.AccountsConfig',
+    'finances.apps.FinancesConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = "accounts.User"
